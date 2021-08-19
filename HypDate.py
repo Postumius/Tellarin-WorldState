3a0f7dd41bf5eecf9fdd37d6376cc72445195f2d
noahma <noahfierz@gmail.com>
2021-08-19 16:39:16 -0700

    Working on day display, can advance to the next day

--- HypDate.py
+++ HypDate.py
@@ -1,7 +1,26 @@
-class HyDate:
+class HypDate:
 
     def __init__(self, year, season, cycle, day):
         self.year = year
         self.season = season 
         self.cycle = cycle
         self.day = day
+
+    def plus(self, other):
+        daySum = self.day + other.day
+        cycleSum = self.cycle + other.cycle + daySum // 10
+        seasonSum = self.season + other.season + cycleSum // 9
+        yearSum = self.year + other.year + seasonSum // 4
+        return HypDate(yearSum, seasonSum % 4, 
+            cycleSum % 9, daySum % 10)
+    
+    def show(self): 
+        return (self.year, self.season, self.cycle, self.day)
+
+  
+    def dayName(self):
+        return ['Silth', 'Est', 'Kross', 'Oust', 'Rest', 'Streth', 
+            'Eith', 'Kesh', 'Orseh', 'Rath'][self.day]
+
+    def seasonName(self):
+        return['Seed', 'Harvest', 'Scorch', 'Frost'][self.season]
--- timeDate.py
+++ timeDate.py
@@ -1,18 +1,21 @@
 from typing import Text
 import PySimpleGUI as sg 
+import HypDate as hd
 
+#dayRadio = [sg.Radio('Day 1', "dayRadio", default=True, key='day1'), sg.Radio('Day 2', "dayRadio", key='day2')]
 
+today = hd.HypDate(0,0,0,0)
 
-dayRadio = [sg.Radio('Day 1', "dayRadio", default=True, key='day1'), sg.Radio('Day 2', "dayRadio", key='day2')]
 controlLayout = [
-    dayRadio,
+    [sg.Text('day:'), sg.Button('Next')],
     [sg.Text('Weather:'), sg.Input(key='weather')],
     [sg.Button('Update'), sg.Button('Close')]
 ]
 controlWindow = sg.Window('Controls', controlLayout)
 
 displayLayout = [ 
-    [sg.Text('Day:'), sg.Text(size=(5,1), key='day')],
+    [sg.Text('Day:'), 
+        sg.Text(today.dayName(), size=(6,1), key='day')],
     [sg.Text('Weather:'), sg.Text(size=(15,1), key='weather')]
 ]
 displayWindow = sg.Window('Display', displayLayout, finalize=True)
@@ -28,8 +31,12 @@ while True:
     event, values = controlWindow.read()
     if event == sg.WIN_CLOSED or event == 'Close': # if user closes window or clicks cancel
         break
+    elif event == 'Next':        
+        today = today.plus(hd.HypDate(0,0,0,1))
+        print(today.show())
+        displayWindow['day'].update(today.dayName())
     else:
-        displayWindow['day'].update(getRadioChoice(controlLayout[0], values))
+        
         displayWindow['weather'].update(values['weather'])
 
 controlWindow.close()
