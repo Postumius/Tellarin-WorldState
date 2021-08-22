5adf0d8e9b124c7576d008133af06b051cbcb8de
noahma <noahfierz@gmail.com>
2021-08-21 18:34:08 -0700

    added multiBaseAddition function

--- HypDate.py
+++ HypDate.py
@@ -1,26 +1,72 @@
+
+def multiBaseAddition(xs, ys, bs):
+
+    def tail(ls):
+        cp = ls.copy()
+        cp.pop(0)
+        return cp
+
+    if len(bs) == 0:
+        return [ xs[0] + ys[0] ]
+    else:
+        rest = multiBaseAddition(
+            tail(xs), tail(ys), tail(bs))
+        return [
+            xs[0] + ys[0] + rest[0] // bs[0],
+            rest[0] % bs[0]
+        ] + tail(rest)
+
+
 class HypDate:
 
-    def __init__(self, year, season, cycle, day):
+    def __init__(
+    self, 
+    year=0, 
+    season=0, 
+    cycle=0, 
+    day=0, 
+    hour=0, 
+    minute=0):
         self.year = year
         self.season = season 
         self.cycle = cycle
         self.day = day
+        self.hour = hour
+        self.minute = minute
 
+    
     def plus(self, other):
-        daySum = self.day + other.day
+        minuteSum = self.minute + other.minute
+        hourSum = self.hour + other.hour + minuteSum // 60
+        daySum = self.day + other.day + hourSum // 24
         cycleSum = self.cycle + other.cycle + daySum // 10
         seasonSum = self.season + other.season + cycleSum // 9
         yearSum = self.year + other.year + seasonSum // 4
         return HypDate(yearSum, seasonSum % 4, 
             cycleSum % 9, daySum % 10)
     
-    def show(self): 
-        return (self.year, self.season, self.cycle, self.day)
+    def toList(self): 
+        return [self.year, self.season, self.cycle, self.day,
+            self.hour, self.minute]
 
-  
+    def seasonName(self):
+        return['Seed', 'Harvest', 'Scorch', 'Frost'][self.season]
+
+    def cycleName(self):
+        return {
+            0 : '1st',
+            1 : '2nd',
+            2 : '3rd'
+        }.get(self.cycle, str(self.cycle + 1) + 'th')
+
     def dayName(self):
         return ['Silth', 'Est', 'Kross', 'Oust', 'Rest', 'Streth', 
             'Eith', 'Kesh', 'Orseh', 'Rath'][self.day]
 
-    def seasonName(self):
-        return['Seed', 'Harvest', 'Scorch', 'Frost'][self.season]
+    def dateName(self):
+        return '{} {} of {}, {} YoR'.format(
+            self.cycleName(), self.dayName(), 
+            self.seasonName(), str(self.year))
+        
+
+    
