268032035f0e0410209c6cffbadaaacc262801c4
noahma <noahfierz@gmail.com>
2021-08-21 21:52:46 -0700

    display working

--- HypDate.py
+++ HypDate.py
@@ -1,22 +1,3 @@
-
-def multiBaseAddition(xs, ys, bs):
-
-    def tail(ls):
-        cp = ls.copy()
-        cp.pop(0)
-        return cp
-
-    if len(bs) == 0:
-        return [ xs[0] + ys[0] ]
-    else:
-        rest = multiBaseAddition(
-            tail(xs), tail(ys), tail(bs))
-        return [
-            xs[0] + ys[0] + rest[0] // bs[0],
-            rest[0] % bs[0]
-        ] + tail(rest)
-
-
 class HypDate:
 
     def __init__(
@@ -33,21 +14,37 @@ class HypDate:
         self.day = day
         self.hour = hour
         self.minute = minute
-
     
     def plus(self, other):
-        minuteSum = self.minute + other.minute
-        hourSum = self.hour + other.hour + minuteSum // 60
-        daySum = self.day + other.day + hourSum // 24
-        cycleSum = self.cycle + other.cycle + daySum // 10
-        seasonSum = self.season + other.season + cycleSum // 9
-        yearSum = self.year + other.year + seasonSum // 4
-        return HypDate(yearSum, seasonSum % 4, 
-            cycleSum % 9, daySum % 10)
+        def multiBaseAddition(xs, ys, bs):
+            def tail(ls):
+                cp = ls.copy()
+                cp.pop(0)
+                return cp
+
+            if len(bs) == 0:
+                return [ xs[0] + ys[0] ]
+            else:
+                rest = multiBaseAddition(
+                    tail(xs), tail(ys), tail(bs))
+                return [
+                    xs[0] + ys[0] + rest[0] // bs[0],
+                    rest[0] % bs[0]
+                ] + tail(rest)
+
+        return HypDate.fromList(multiBaseAddition(
+            self.toList(),
+            other.toList(),
+            [4, 9, 10, 24, 60]
+        ))
     
     def toList(self): 
         return [self.year, self.season, self.cycle, self.day,
             self.hour, self.minute]
+
+    @staticmethod
+    def fromList(ls):        
+        return HypDate(ls[0], ls[1], ls[2], ls[3], ls[4], ls[5])
 
     def seasonName(self):
         return['Seed', 'Harvest', 'Scorch', 'Frost'][self.season]
@@ -67,6 +64,12 @@ class HypDate:
         return '{} {} of {}, {} YoR'.format(
             self.cycleName(), self.dayName(), 
             self.seasonName(), str(self.year))
+
+    def timeName(self):
+        return '{}:{}'.format(
+            str(self.hour).zfill(2),
+            str(self.minute).zfill(2)
+        )
         
 
     
--- timeDate.py
+++ timeDate.py
@@ -1,5 +1,6 @@
 from typing import Text
-import PySimpleGUI as sg 
+import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import Button 
 import HypDate as hd
 
 #dayRadio = [sg.Radio('Day 1', "dayRadio", default=True, key='day1'), sg.Radio('Day 2', "dayRadio", key='day2')]
@@ -7,37 +8,93 @@ import HypDate as hd
 today = hd.HypDate(0,0,0,0)
 
 controlLayout = [
-    [sg.Text('day:'), sg.Button('Next')],
-    [sg.Text('Weather:'), sg.Input(key='weather')],
+    [sg.Text('year:'), sg.Input(size=(5,1), key='year')],
+    [sg.Text('season:'), sg.Input(size=(5,1), key='season'),
+     sg.Text(today.seasonName(), size=(6,1), key='seasonName')],
+    [sg.Text('cycle:'), sg.Input(size=(5,1), key='cycle')],
+    [sg.Text('day:'), sg.Input(size=(5,1), key='day'),
+     sg.Text(today.dayName(), size=(6,1), key='dayName'),
+     sg.Button('Prev'), sg.Button('Next')],
+    [sg.Text('hour:'), sg.Input(size=(5,1), key='hour')],
+    [sg.Text('Weather:'), sg.Input(size=(25,1), key='weather')],
+    [sg.Text('Location:'), sg.Input(size=(25,1), key='location')],
     [sg.Button('Update'), sg.Button('Close')]
 ]
 controlWindow = sg.Window('Controls', controlLayout)
 
-displayLayout = [ 
-    [sg.Text('Day:'), 
-        sg.Text(today.dayName(), size=(6,1), key='day')],
-    [sg.Text('Weather:'), sg.Text(size=(15,1), key='weather')]
+displayColumn = [
+    [sg.Text('Date:'), 
+     sg.Text(today.dateName(), size=(25,1), key='date')],
+    [sg.Text('Hour:'),
+     sg.Text(str(today.hour), size=(25,1), key='hour')],
+    [sg.Text('Weather:'), sg.Text(size=(25,1), key='weather')],
+    [sg.Text('Location:'), sg.Text(size=(25,1), key='location')]
 ]
-displayWindow = sg.Window('Display', displayLayout, finalize=True)
 
-#print(controlLayout[0][0].Key)
+displayLayout = [ 
+    [sg.Text(key='-EXPAND-', font='ANY 1', pad=(0, 0))],
+    [sg.Column(displayColumn, vertical_alignment='center', key='-C-')]
+]
 
-def getRadioChoice(radioList, values):
-    for radio in radioList:
-        if values[radio.Key] == True:
-            return radio.Text
+displayWindow = sg.Window('Display', displayLayout, 
+    finalize=True, resizable=True, 
+    font=('helvetica', 50),
+    auto_size_text=True,
+    element_padding=(0,15))
+
+displayWindow['-C-'].expand(True, True, True)
+displayWindow['-EXPAND-'].expand(True, True, True)
 
 while True:
     event, values = controlWindow.read()
-    if event == sg.WIN_CLOSED or event == 'Close': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Close':
         break
     elif event == 'Next':        
-        today = today.plus(hd.HypDate(0,0,0,1))
-        print(today.show())
-        displayWindow['day'].update(today.dayName())
-    else:
+        today = today.plus(hd.HypDate(day=1))  
+    elif event == 'Prev':        
+        today = today.plus(hd.HypDate(day=-1))
         
-        displayWindow['weather'].update(values['weather'])
+    else:
+        try:
+            today.year = int(values['year'])
+        except: 
+            today.year = 0
+
+        try:
+            today.season = int(values['season'])-1
+        except: 
+            today.season = 0
+
+        try:
+            today.cycle = int(values['cycle'])-1
+        except: 
+            today.cycle = 0
+
+        try:
+            today.day = int(values['day'])-1
+        except: 
+            today.day = 0  
+
+        try:
+            today.hour = int(values['hour'])
+        except: 
+            today.hour = 0      
+    
+    today = today.plus(hd.HypDate())
+    controlWindow['year'].update(str(today.year))
+    controlWindow['seasonName'].update(today.seasonName())
+    controlWindow['season'].update(str(today.season+1))
+    controlWindow['cycle'].update(str(today.cycle+1))
+    controlWindow['dayName'].update(today.dayName())
+    controlWindow['day'].update(str(today.day+1))
+    controlWindow['hour'].update(str(today.hour))
+
+    displayWindow['date'].update(today.dateName())
+    displayWindow['hour'].update(str(today.hour))
+    displayWindow['weather'].update(values['weather'])
+    displayWindow['location'].update(values['location'])
+
+    print(today.toList())
 
 controlWindow.close()
 displayWindow.close()
